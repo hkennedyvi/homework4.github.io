0b023cf9eb16ca69b85922409cea8ceef29d4d0e
Howard Kennedy <hkennedyvi@gmail.com>
2019-11-14 12:45:05 -0800

    Styling changes

--- questions.js
+++ questions.js
@@ -11,3 +11,31 @@ var questions = [
     },
     ///etc.
   ];
+
+
+
+var questionQuiz = document.querySelector(".ask");
+
+askAway();
+
+function askAway() {
+  for (var i = 0; i < questions.length; i++) {
+  questionQuiz.innerHTML = questions[1].title;
+}
+
+
+/*
+  var themeSwitcher = document.querySelector("#theme-switcher");
+var container = document.querySelector(".container");
+var mode = "dark";
+
+themeSwitcher.addEventListener("click", function() {
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
