cd5f8e3df53ed82a13d5b0f24216d8b7c49de411
Howard Kennedy <hkennedyvi@gmail.com>
2019-11-14 11:24:12 -0800

    2nd html added, revisions made

--- script.js
+++ script.js
@@ -10,12 +10,38 @@ var timeEl = document.querySelector("#timer");
 var secondsLeft = 76;
 
 function startTimer() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left in quiz";
 
-        if(secondsLeft === 0) {
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
         }
     }, 1000);
 }
+
+
+
+
+
+
+
+
+
+
+var score = 0;
+
+function runQuiz() {
+for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt);
+
+    if (response == questions[i].answer) {
+        score++;
+        alert("CORRECT");
+    } else {
+        alert("WRONG");
+    }
+}
+};
+
+alert("You got " + score + "/" + questions.length);
